4339fd2d5784839017bf541725126b42ebab0129
Jan Lesko <jan.lesko.sk@gmail.com>
2016-10-21 12:17:18 +0000

    Add new exercise files

---
+++ rubybook1/4_flow_control/1_true_false.rb
@@ -0,0 +1,14 @@
+print "(32 * 4) >= 129 ... "
+(32 * 4) >= 129 ? puts("true") : puts("false")
+
+print "false != !true ... "
+false != !true ? puts("true") : puts("false")
+
+print "true == 4 ... "
+true == 4 ? puts("true") : puts("false")
+
+print "false == (847 == '874') ... "
+false == (847 == '874') ? puts("true") : puts("false")
+
+print "(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false ... "
+(!true || (!(100 / 5) == 20) || ((328 / 4) == 82)) || false ? puts("true") : puts("false")
