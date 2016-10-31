2fcc70e9eb1df9069740cce1ad027f4e02bed085
Jan Lesko <jan.lesko.sk@gmail.com>
2016-10-31 23:16:09 +0100

    updated loan_calculator

--- lesson-2/loan_calculator.rb
+++ lesson-2/loan_calculator.rb
@@ -1,62 +1,63 @@
 def valid_number?(number)
-  (number.to_i.to_s == number || number.to_f.to_s == number) && number.to_i > 0
+  (number.to_i.to_s == number || number.to_f.to_s == number) &&
+    (number.to_i >= 0 || number.to_f >= 0)
 end
 
-loop do
-  puts "Hello, welcome to Loan Calculator!"
-  puts "It will help you determine the monthly payments on a loan."
+puts "Welcome to Loan Calculator!"
+puts "It will calculate the monthly payments on a loan."
 
+loop do
   loan_amount = nil
   loop do
     puts "Enter the loan amount"
     loan_amount = gets.chomp
     break if valid_number?(loan_amount)
     puts "That is not a valid number."
-    puts "Please enter the loan amount in the following example format: 10000"
   end
 
-  apr = nil
+  annual_perc_rate = nil
   loop do
     puts "Enter the Annual Percentage Rate (APR)"
-    apr = gets.chomp
-    break if valid_number?(apr)
+    annual_perc_rate = gets.chomp
+    break if valid_number?(annual_perc_rate)
     puts "That is not a valid number."
   end
 
   loan_duration_years = nil
   loan_duration_months = nil
   loop do
-    puts "Select if you wish to enter the loan duration in months or years."
-    puts "Hit \"M\" for months or \"Y\" for years."
-    choice = gets.chomp.downcase
-
-    if choice == "m"
-      puts "Enter the loan duration in months"
-      loan_duration_months = gets.chomp
-    elsif choice == "y"
-      puts "Enter the loan duration in years"
-      loan_duration_years = gets.chomp
-    end
-
-    break if valid_number?(loan_duration_months) ||
+    puts "Select the loan duration in months"
+    loan_duration_months = gets.chomp
+    puts "Select the loan duration in years"
+    loan_duration_years = gets.chomp
+    break if valid_number?(loan_duration_months) &&
              valid_number?(loan_duration_years)
-    puts "That is not a valid choice or number."
+    puts "That is not a valid number."
   end
 
-  apr = apr.to_f / 100
-  mir = apr / 12
+  annual_perc_rate = annual_perc_rate.to_f / 100
+  monthly_perc_rate = annual_perc_rate / 12
   loan_duration_months = loan_duration_months.to_i +
                          loan_duration_years.to_i * 12
 
-  monthly_payment = loan_amount.to_f *
-                    (mir / (1 - (1 + mir)**-loan_duration_months))
+  if monthly_perc_rate.zero?
+    monthly_payment = loan_amount.to_f / loan_duration_months
+  else
+    monthly_payment = loan_amount.to_f * (monthly_perc_rate /
+                      (1 - (1 + monthly_perc_rate)**-loan_duration_months))
+  end
 
-  puts "Your monthly payments is $#{format('%02.2f', monthly_payment)}."
+  if loan_duration_months.zero?
+    puts "The loan of $" + loan_amount +
+         " needs to be paid back as soon as it is received."
+  else
+    puts "Your monthly payments is $#{format('%02.2f', monthly_payment)}."
+  end
 
-  puts "You you with to start calculation again?"
+  puts "Do you wish to start calculation again? Press \"Y\" to confirm."
   choice = gets.chomp
 
-  break unless choice.downcase.start_with?('y')
+  break unless choice.casecmp == "y"
 end
 
 puts "Thank you for using the Loan Calculator!"
