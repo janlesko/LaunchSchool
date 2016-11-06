108c191bc1bdc34a02078552902b64a06229c09a
Jan Lesko <jan.lesko.sk@gmail.com>
2016-11-06 21:01:59 +0000

    New exercise files

---
+++ lesson-3/medium-1/q6.rb
@@ -0,0 +1,16 @@
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+#input_array is modified
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size >= max_buffer_size
+  buffer
+end
+
+#input_array is not altered
+
---
+++ lesson-3/medium-1/q8.rb
@@ -0,0 +1,1 @@
+puts "I'm good, thank you for asking".split.map { |word| word.capitalize }.join(" ")
