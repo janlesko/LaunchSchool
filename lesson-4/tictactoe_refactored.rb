c979e51403e4adc5aaa67c1eed1eeceb7070ffb7
Jan Lesko <jan.lesko.sk@gmail.com>
2016-11-11 00:41:43 +0100

    new files

--- lesson-4/tictactoe_refactored.rb
+++ lesson-4/tictactoe_refactored.rb
@@ -1,6 +1,7 @@
 INITIAL_MARKER = " ".freeze
 PLAYER_MARKER = "X".freeze
 COMPUTER_MARKER = "O".freeze
+WINNER_SCORE = 5.freeze
 
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
@@ -13,7 +14,7 @@ end
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
-  puts " You're a X. Computer is O."
+  puts " You're a X. Computer is O. First to 5 wins."
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -40,10 +41,21 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
 def player_places_piece!(brd)
   square = ''
   loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    prompt "Choose a square #{joinor(empty_squares(brd), ', ')}"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice"
@@ -66,20 +78,18 @@ def someone_won?(brd)
 end
 
 def detect_winner(brd)
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
+  WINNING_LINES.each do |line|
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
       return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
       return 'Computer'
     end
   end
   nil
 end
 
+player_score = 0
+computer_score = 0
 loop do
   board = initialize_board
 
@@ -101,9 +111,24 @@ loop do
     prompt "It's a tie!"
   end
 
-  prompt "Play again? (y or n)"
+  if detect_winner(board) == 'Player'
+    player_score += 1
+  elsif detect_winner(board) == 'Computer'
+    computer_score += 1
+  end
+
+  prompt "Player score is: #{player_score} / Computer score is: #{computer_score}"
+  prompt "Press enter to continue"
+  confirm = gets.chomp
+
+  next unless player_score == WINNER_SCORE || computer_score == WINNER_SCORE
+  prompt "Player wins!" if player_score == WINNER_SCORE
+  prompt "Computer wins!" if computer_score == WINNER_SCORE
+  prompt "Play agin? (y or n)"
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
+  player_score = 0
+  computer_score = 0
 end
 
 prompt "Thanks for palying TicTacToe"
