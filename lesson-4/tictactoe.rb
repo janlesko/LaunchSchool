d12e4c2af2ed68e9da5c18e6d4c493f6632dac41
Jan Lesko <jan.lesko.sk@gmail.com>
2016-11-13 23:22:27 +0100

    new project

--- lesson-4/tictactoe.rb
+++ lesson-4/tictactoe.rb
@@ -1,10 +1,12 @@
-INITIAL_MARKER = " ".freeze
-PLAYER_MARKER = "X".freeze
-COMPUTER_MARKER = "O".freeze
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
+WHO_FIRST = 'choose'.freeze
+WINNER_SCORE = 5
 
-WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
+WINNING_LANES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +
-                [[1, 5, 9], [3, 5, 7]].freeze
+                [[1, 5, 9], [3, 5, 7]]
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -13,7 +15,7 @@ end
 # rubocop:disable Metrics/AbcSize
 def display_board(brd)
   system 'clear'
-  puts " You're a X. Computer is O."
+  puts "You're the \"#{PLAYER_MARKER}\". Computer is the \"#{COMPUTER_MARKER}\""
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -26,9 +28,32 @@ def display_board(brd)
   puts "     |     |"
   puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
   puts "     |     |"
-  puts ""
 end
-# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/Abcsize
+
+def first_turn
+  if WHO_FIRST == "player"
+    "player"
+  elsif WHO_FIRST == "computer"
+    "computer"
+  else
+    user_chooses_first
+  end
+end
+
+def user_chooses_first
+  loop do
+    prompt "Choose who goes first: (P)layer, (C)omputer, (R)andom."
+    answer = gets.chomp.upcase
+    case answer
+    when "P" then return "player"
+    when "C" then return "computer"
+    when "R" then return ["player", "computer"].sample
+    else
+      prompt "not a valid choice"
+    end
+  end
+end
 
 def initialize_board
   new_board = {}
@@ -40,70 +65,143 @@ def empty_squares(brd)
   brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
-def player_places_piece!(brd)
-  square = ''
-  loop do
-    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
-    square = gets.chomp.to_i
-    break if empty_squares(brd).include?(square)
-    prompt "Sorry, that's not a valid choice"
-  end
-
-  brd[square] = PLAYER_MARKER
-end
-
-def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
-  brd[square] = COMPUTER_MARKER
-end
-
 def board_full?(brd)
   empty_squares(brd).empty?
 end
 
-def someone_won?(brd)
-  !!detect_winner(brd)
+def joinor(arr, deliminator=', ', word='or')
+  case arr.size
+  when 0 then ""
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(deliminator)
+  end
+end
+
+def player_pick_square!(brd)
+  answer = ''
+  loop do
+    prompt "Select square: #{joinor(empty_squares(brd))}"
+    answer = gets.chomp.to_i
+    break if empty_squares(brd).include?(answer)
+    prompt "Not a valid choice"
+  end
+  brd[answer] = PLAYER_MARKER
+end
+
+def computer_pick_square!(brd)
+  square = attack_move(brd)
+  square = defense_move(brd) if !square
+  square = take_middle(brd) if !square
+  square = empty_squares(brd).sample if !square
+  brd[square] = COMPUTER_MARKER
+end
+
+def find_at_risk_square(line, brd, marker)
+  if brd.values_at(*line).count(marker) == 2
+    brd.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
+  end
+end
+
+def attack_move(brd)
+  square = nil
+  WINNING_LANES.each do |line|
+    square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+    break if square
+  end
+  square
+end
+
+def defense_move(brd)
+  square = nil
+  WINNING_LANES.each do |line|
+    square = find_at_risk_square(line, brd, PLAYER_MARKER)
+    break if square
+  end
+  square
+end
+
+def take_middle(brd)
+  5 if empty_squares(brd).include?(5)
 end
 
 def detect_winner(brd)
-  winning_lines.each do |line|
-    if brd[line[0]] == PLAYER_MARKER &&
-       brd[line[1]] == PLAYER_MARKER &&
-       brd[line[2]] == PLAYER_MARKER
-      return 'Player'
-    elsif brd[line[0]] == COMPUTER_MARKER &&
-          brd[line[1]] == COMPUTER_MARKER &&
-          brd[line[2]] == COMPUTER_MARKER
-      return 'Computer'
+  WINNING_LANES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return "Player"
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return "Computer"
     end
   end
   nil
 end
 
+def display_winner(brd)
+  if detect_winner(brd) == 'Player'
+    prompt "Player won"
+  elsif detect_winner(brd) == 'Computer'
+    prompt "Computer won"
+  elsif board_full?(brd)
+    prompt "It's a tie"
+  end
+end
+
+def somebody_won?(brd)
+  !!detect_winner(brd)
+end
+
+def play_again?
+  loop do
+    prompt "Play again? y/n"
+    answer = gets.chomp.downcase
+    if answer == 'y'
+      return true
+    elsif answer == 'n'
+      return false
+    else
+      prompt "Not a valid choice."
+    end
+  end
+end
+
+def place_piece!(brd, current_player)
+  if current_player == "computer"
+    computer_pick_square!(brd)
+  else
+    player_pick_square!(brd)
+  end
+end
+
+def alternate_player(current_player)
+  current_player == "player" ? "computer" : "player"
+end
+
+computer_score = 0
+player_score = 0
+
 loop do
   board = initialize_board
-
+  current_player = first_turn
   loop do
     display_board(board)
-
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
-
-    computer_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+    place_piece!(board, current_player)
+    current_player = alternate_player(current_player)
+    break if board_full?(board) || somebody_won?(board)
   end
 
   display_board(board)
+  display_winner(board)
 
-  if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
-  else
-    prompt "It's a tie!"
-  end
+  computer_score += 1 if detect_winner(board) == 'Computer'
+  player_score += 1 if detect_winner(board) == 'Player'
 
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  prompt "Computer score is: #{computer_score}"
+  prompt "Player score is: #{player_score}"
+
+  break if computer_score == 5 || player_score == 5
+  break unless play_again?
 end
 
-prompt "Thanks for palying TicTacToe"
+prompt "Game over. Thank you for playing."
