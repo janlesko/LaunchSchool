a8f64fafb77d92e457e57a4f96ffd711fa0ba16c
Jan Lesko <jan.lesko.sk@gmail.com>
2017-07-23 12:15:22 +0200

    complete users-and-interests

--- 170_Web_Development/04_Working_With_Sinatra/users-and-interests/user_and_interests.rb
+++ 170_Web_Development/04_Working_With_Sinatra/users-and-interests/user_and_interests.rb
@@ -13,11 +13,26 @@ get "/" do
   erb :home
 end
 
-get "user/:name" do
-  user = params[:name]
+get "/user/:name" do
+  user = params[:name].to_sym
 
-  @email = @content.fetch(user.to_sym)[:email]
-  @interests = @content.fetch(user.to_sym)[:interests]
+  @email = @content.fetch(user)[:email]
+  @interests = @content.fetch(user.to_sym)[:interests].join(", ")
+  @other_names = @content.keys - [user]
 
   erb :user
+end
+
+helpers do
+  def users_count
+    @content.keys.count
+  end
+
+  def interests_count
+    result = 0
+    @content.each do |_, details|
+      result += details[:interests].count
+    end
+    result
+  end
 end
