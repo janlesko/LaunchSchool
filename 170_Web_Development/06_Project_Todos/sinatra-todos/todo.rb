6cdfe0cb962ffe6e8951c3f5a67661f2c2cdf436
Jan Lesko <jan.lesko.sk@gmail.com>
2017-08-06 23:17:27 +0200

    add initial sinatra todos

---
+++ 170_Web_Development/06_Project_Todos/sinatra-todos/todo.rb
@@ -0,0 +1,15 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+get "/" do
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = [
+      {name: "Lunch Groceries", todos: []},
+      {name: "Dinner Groceries", todos: []}
+  ]
+  erb :lists, layout: :layout
+end
